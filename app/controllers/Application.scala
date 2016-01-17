078532bc1cc628aa6d649e1f23cd989d806bcc29
Francis Devereux <francis@bright-interactive.co.uk>
2016-01-17 21:30:27 +0000

    Add task form by following tuturial

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,26 @@
 package controllers
 
-import play.api._
+import models.Task
+import play.api.data.Forms._
+import play.api.data._
 import play.api.mvc._
 
 class Application extends Controller {
 
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+
   def index = Action {
-    Ok(views.html.index("Hello world!"))
+    Redirect(routes.Application.tasks)
   }
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+   }
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 
 }
